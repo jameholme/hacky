1a80cef21903109d86a41c535865e91b5815c195
jameholme <39424878+jameholme@users.noreply.github.com>
2020-06-04 16:44:53 -0400

    Update shodan_scan.sh

--- shodan_scan.sh
+++ shodan_scan.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#Scans multiple hsots in a target list using Shodan (requires an initialized Shodan API Key)
+
 for host in $(cat target_list.txt)
 do
-    shodan host $host >> ~/Desktop/Projects/Neptune/shodan_results.txt
+    shodan host $host >> ~/Desktop/shodan_results.txt
     sleep 1
 done
