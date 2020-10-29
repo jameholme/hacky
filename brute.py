29478fec0c60c9eb149e904471140adbcb19d23c
jameholme <39424878+jameholme@users.noreply.github.com>
2020-10-29 08:03:19 -0400

    Update and rename brute_pin.py to brute.py

--- brute.py
+++ brute.py
@@ -9,7 +9,7 @@ last_name_file = '/usr/share/wordlists/seclists/Usernames/Names/lastname.txt'
 pin_file = '/usr/share/wordlists/seclists/Fuzzing/pins.txt'
 
 # Define the brute force function
-def brute_pin():
+def brute():
     with open(first_name_file) as first_names, open(last_name_file) as last_names, open(pin_file) as pins:
     first_name = [line.rstrip() for line in first_names]
     last_name = [line.rstrip() for line in last_names]
@@ -26,4 +26,4 @@ def brute_pin():
                     print(str(response.status_code) + " Status Code :: " + url_request + " :: Something Found.")
                     break
 # Run the brute force function
-brute_pin()
+brute()
